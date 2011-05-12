d56e0160f75933ac930f1821626c336f50610ff4
Fredrik Rubensson <fredrik@eldfluga.se>
2011-05-12 20:05:20 +0200

    made the test work and added jquery (removed others)

--- features/step_definitions/inline_steps.rb
+++ features/step_definitions/inline_steps.rb
@@ -1,5 +1,5 @@
-When /^I click on the element with id "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I click on the element with id "([^"]*)"$/ do |id|
+  find_by_id(id).click
 end
 
 When /^I fill in the editor with "([^"]*)"$/ do |arg1|
