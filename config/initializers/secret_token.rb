1b126ed6fd959e28cc75f5f61327a9155f8028ea
Fredrik Rubensson <fredrik@eldfluga.se>
2011-05-12 14:36:44 +0200

    new rails project to demonstrate inline editing with tinymce

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inlinetinymce::Application.config.secret_token = 'f8c2db36d0de5af4969177e510467fdf4d0b58896ea8e37c6326edd4f3cb9d371f3bc84c1ed3662ff3c86e754d4ffe299c765dcedc5e950b08c8477ff40e65aa'
